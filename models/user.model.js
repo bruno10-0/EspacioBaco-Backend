b6339bfe6814a8c703920c74403484f4f77b8f4e
bruno10-0 <brunobrodon75@gmail.com>
2024-03-14 04:24:53 -0300

    feat: :sparkles: seteamos por defecto el valor de tipo de usuario a normal

--- models/user.model.js
+++ models/user.model.js
@@ -1,54 +1,55 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../utils/sequelize.js";
 
-export const Usuario = sequelize.define('Usuario', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
+export const Usuario = sequelize.define("Usuario", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  tipo: {
+    type: DataTypes.ENUM("admin", "normal"),
+    defaultValue: "normal",
+    allowNull: false,
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  apellido: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  correo: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      isEmail: true,
     },
-    tipo: {
-      type: DataTypes.ENUM('admin', 'normal'),
-      allowNull: false
-    },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    apellido: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    correo: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
-    },
-    contrasenia: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    telefono: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    direccion: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    createdAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW,
-      },
-  });
-  Usuario.sync()
+  },
+  contrasenia: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  telefono: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  direccion: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW,
+  },
+});
+Usuario.sync()
   .then(() => {
     console.log(
       'Modelo "Usuario" sincronizado correctamente con la base de datos.'
